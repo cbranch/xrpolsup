e68155fccf64bc856a80083e850b8dd2494c0e69
Chris Branch <chris@chrisbranch.co.uk>
2024-07-13 22:14:54 +0100

    Allow superusers to view all reports and releases from all time

--- backoffice-frontend/src/App.vue
+++ backoffice-frontend/src/App.vue
@@ -87,8 +87,7 @@ export default {
         } else if (jwRes.statusCode == 200) {
           this.$store.commit('logIn', resData)
 
-          var threshold = Date.now() - (1000*60*60*24*28)
-          this.$io.socket.get('/api/v1/report', {limit: 10000, where: {updatedAt: {">": threshold}}}, (resData, jwRes) => {
+          this.$io.socket.get('/api/v1/report', {limit: 10000}, (resData, jwRes) => {
             if (jwRes.statusCode != 200) {
               return
             }
